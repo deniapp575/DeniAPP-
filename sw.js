6669e3f91d691634aa4dfe789ccf7bf216435b48
deniapp575 <thelittlesucculentshack@yahoo.com>
2026-01-12 17:33:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
+const CACHE_NAME = 'denia-app-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install and Cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Serve files from Cache when offline
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
